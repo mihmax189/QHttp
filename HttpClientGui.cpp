7e3fe96f5ce5d238b7972ba0e8be98310543363c
Max Miheev <mihmax189@gmail.com>
2014-07-31 21:29:50 +0400

    Определение метода HttpClientGui::slotGo()

--- HttpClientGui.cpp
+++ HttpClientGui.cpp
@@ -2,6 +2,11 @@
 #include "HttpClientGui.hpp"
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
+#include <QLabel>
+#include <QLineEdit>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QGridLayout>
 
 HttpClient::HttpClient(QObject * pobj) : QObject(pobj) {
   const QString serverLink = "http://s5.goodfon.ru/"; //"http://cbr.ru";
@@ -14,8 +19,9 @@ HttpClient::HttpClient(QObject * pobj) : QObject(pobj) {
 
 void HttpClient::download(const QUrl & url) {
   QNetworkRequest request(url);                           // входной объект запроса для pReply
-  /*QNetworkReply * pReply =*/ pNetManager->get(request); // выполнение запроса
-  //connect(pReply, SIGNAL())
+  QNetworkReply * pReply = pNetManager->get(request); // выполнение запроса
+  connect(pReply, SIGNAL(downloadProgress(qint64, qint64)),
+          this, SIGNAL(downloadProgress(qint64, qint64)));
 }
 
 
@@ -26,4 +32,38 @@ void HttpClient::slotFinished(QNetworkReply * pReply) {
   else
     emit done(pReply->url(), pReply->readAll());          // передать полученные данные дальше
   pReply->deleteLater();                                  // удаление объекта QNetworkReply
+}
+
+// HttpClientGui methods
+HttpClientGui::HttpClientGui(QWidget * pwgt) : QWidget(pwgt) {
+  pClient = new HttpClient(this);
+  pProgBar = new QProgressBar;
+  pHeaderRateDollar = new QLabel("Rate of Dolllar: ");
+  pHeaderRateEuro = new QLabel("Rate of Euro: ");
+  pRateDollarLineEdit = new QLineEdit;
+  pRateDollarLineEdit->setReadOnly(true);
+  pRateEuroLineEdit = new QLineEdit;
+  pRateEuroLineEdit->setReadOnly(true);
+  pGoButton = new QPushButton("Go");
+  strDownloadLink = "http://cbr.ru/scripts/XML_daily.asp";
+
+  //connect(pGoButton, SIGNAL(clicked()), SLOT(slotGo()));
+  //connect(pClient, SIGNAL(downloadProgress(qint64, qint64)),
+  //        this, SLOT(slotDownloadProgress(qint64, qint64)));
+  //connect(pClient, SIGNAL(done(const QUrl &, const QByteArray &)),
+  //        this, SLOT(slotDone(const QUrl &, const QByteArray &)));
+
+  QGridLayout * pMainLayout = new QGridLayout;
+  pMainLayout->addWidget(pHeaderRateDollar, 0, 0);
+  pMainLayout->addWidget(pRateDollarLineEdit, 0, 1);
+  pMainLayout->addWidget(pHeaderRateEuro, 1, 0);
+  pMainLayout->addWidget(pRateEuroLineEdit, 1, 1);
+  pMainLayout->addWidget(pProgBar, 2, 0);
+  pMainLayout->addWidget(pGoButton, 3, 0);
+
+  setLayout(pMainLayout);
+}
+
+void HttpClientGui::slotGo() {
+  pClient->download(QUrl(strDownloadLink));
 }
--- HttpClientGui.hpp
+++ HttpClientGui.hpp
@@ -1,7 +1,6 @@
 #ifndef HTTP_CLIENT_H_
 #define HTTP_CLIENT_H_
 
-//#include <QGui>
 #include <QHttp>
 #include <QObject>
 #include <QUrl>
@@ -11,19 +10,25 @@
 
 class QNetworkAccessManager;
 class QNetworkReply;
+class QProgressBar;
+class QLineEdit;
+class QLabel;
+class QPushButton;
 
 class HttpClient : public QObject {
 Q_OBJECT;
 private:
   QHttp httpServer;
   QNetworkAccessManager * pNetManager;
-  void download(const QUrl &);                 // загрузка ресурса, заданного в параметр QUrl
+
 public:
   HttpClient(QObject * pwgt = 0);
+  void download(const QUrl &);                 // загрузка ресурса, заданного в параметр QUrl
 
 signals:
   void done(const QUrl &, const QByteArray &); // сигнал о завершении загрузки
   void error();                                // сигнал о ошибке загрузки
+  void downloadProgress(qint64, qint64);
 
 private slots:
   void slotFinished(QNetworkReply *);          // вызывается по завершении операции загрузки
@@ -31,7 +36,28 @@ private slots:
 
 
 // реализация GUI
-class HttpClientGui {
+class HttpClientGui : public QWidget {
+  Q_OBJECT
+private:
+  HttpClient * pClient;
+  QProgressBar * pProgBar;
+  QLineEdit * pRateDollarLineEdit;
+  QLineEdit * pRateEuroLineEdit;
+  QLabel * pHeaderRateDollar;
+  QLabel * pHeaderRateEuro;
+  QPushButton * pGoButton;
+  QString strDownloadLink;
+  //void showPic(const QString &);
 
+public:
+  HttpClientGui(QWidget * pwgt = 0);
+
+private slots:
+  void slotGo();                                   // осуществляет испольнение загрузки ресурса
+  //void slotError();                                // вызывает при возникновении ошибки
+  //void slotDownloadProgress(qint64, qint64);       // отображает процесс загрузки
+  //void sloteDone(const QUrl &, const QByteArray);  // выполняет конечные действия по окончании
+                                                     // загрузки
 };
+
 #endif // HTTP_CLIENT_H_
