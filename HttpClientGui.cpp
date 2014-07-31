c178804d201f9ecd63609c8ae109c9f466355830
Max Miheev <mihmax189@gmail.com>
2014-07-31 20:27:09 +0400

    Определение метода HttpClient::download()

--- HttpClientGui.cpp
+++ HttpClientGui.cpp
@@ -7,10 +7,17 @@ HttpClient::HttpClient(QObject * pobj) : QObject(pobj) {
   const QString serverLink = "http://s5.goodfon.ru/"; //"http://cbr.ru";
   // /scripts/XML_daily.asp
   pNetManager = new QNetworkAccessManager(this);
+  // сигнал finished() отправляет по окончании загрузки
   connect(pNetManager, SIGNAL(finished(QNetworkReply *)),
           this, SLOT(slotFinished(QNetworkReply *)));
 }
 
 void HttpClient::slotFinished(QNetworkReply *) {
+  // объект QNetworkReply соответствует текущей проверенной операции
+}
 
+void HttpClient::download(const QUrl & url) {
+  QNetworkRequest request(url);                       // входной объект запроса для pReply
+  /*QNetworkReply * pReply =*/ pNetManager->get(request); // выполнение запроса
+  //connect(pReply, SIGNAL())
 }
